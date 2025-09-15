3f82691d2ff549f4d9b6eb2da9322fa6229d9823
Juan Navarro <juanm.navarro05@gmail.com>
2025-09-15 01:58:03 -0300

    ta quedando increible, de momento solo hmtl y css, falta logica, vamos aplicando js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// --- Constantes y selectores ---
+const calculatorForm = document.getElementById('calculatorForm');
+const inputA = document.getElementById('valorA');
+const inputB = document.getElementById('valorB');
+const selectOperacion = document.getElementById('operacion');
+const resultadoDiv = document.getElementById('resultado');
+const historyList = document.getElementById('historyList');
+const clearHistoryBtn = document.getElementById('clearHistoryBtn');
+const exportJsonBtn = document.getElementById('exportJsonBtn');
+const limpiarFormBtn = document.getElementById('limpiarFormBtn');
+
+
