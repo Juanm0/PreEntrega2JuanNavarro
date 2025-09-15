20f9c192371195908e42ae07957a992a0ac6858a
Juan Navarro <juanm.navarro05@gmail.com>
2025-09-15 02:42:22 -0300

    trabajo finalizado

--- js/script.js
+++ js/script.js
@@ -190,7 +190,7 @@ exportJsonBtn.addEventListener('click', function () {
 });
 
 
-// opcional: cargar historial al inicio (ya ejecutado arriba). Acá se ofrece una función pública por si se necesita re-render
+// opcional: cargar historial al inicio (ya ejecutado arriba). 
 window.reloadHistory = function () {
     historial = loadHistoryFromStorage();
     renderHistory(historial);
